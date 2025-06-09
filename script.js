e163156d1ae16f105717269f80c5c6dcfaa07605
Ernest Javier <ernestjavier6@gmail.com>
2025-06-09 10:13:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,8 @@
-document.querySelectorAll('a.nav-link[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-    const targetId = this.getAttribute("href").replace("#", "");
-    const section = document.getElementById(targetId);
-
-    if (section) {
-      section.scrollIntoView({
-        behavior: "smooth"
-      });
-    }
-  });
-});
-
-document.querySelectorAll('.navbar-nav .nav-link').forEach(link => {
-  link.addEventListener('click', () => {
-    const navbarCollapse = document.querySelector('.navbar-collapse');
-    if (navbarCollapse.classList.contains('show')) {
-      new bootstrap.Collapse(navbarCollapse).hide();
-    }
-  });
-});
-
-document.querySelectorAll('a.nav-link[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-    const targetId = this.getAttribute("href").replace("#", "");
-    const section = document.getElementById(targetId);
-    if (section) {
-      section.scrollIntoView({
-        behavior: "smooth"
-      });
-    }
-  });
-});
-
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.querySelector("#contact form");
   form.addEventListener("submit", function (e) {
     e.preventDefault();
     alert("Pesan terkirim");
     form.reset();
-  });
-});
-
-document.querySelectorAll('.navbar-nav .nav-link').forEach(link => {
-  link.addEventListener('click', () => {
-    const navbarCollapse = document.querySelector('.navbar-collapse');
-    if (navbarCollapse.classList.contains('show')) {
-      new bootstrap.Collapse(navbarCollapse).hide();
-    }
   });
 });

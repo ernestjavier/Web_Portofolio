9d300dd15ade967c8182db47cddc074f7fe79104
Ernest Javier <ernestjavier6@gmail.com>
2025-06-20 14:48:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ document.addEventListener("DOMContentLoaded", function () {
     form.reset();
   });
 });
+
+document.querySelectorAll('a.nav-link').forEach(link => {
+  link.addEventListener('click', function (e) {
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      e.preventDefault();
+      target.scrollIntoView({
+        behavior: "smooth"
+      });
+    }
+  });
+});
